b3d5db0b82bfcbc0fcd22cab3756d786c43a2e51
Yuri-Costa09 <yurircostawork@gmail.com>
2025-07-14 15:38:42 -0300

    refactor of binary search algorithm

--- DSA/algorithms/binary-search.py
+++ DSA/algorithms/binary-search.py
@@ -1,25 +1,23 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-target = 10
+target = 9
 
-def binary_search(array):
+def binary_search(array, target):
     low = 0
     high = len(array) - 1
     center = (low + high) // 2
 
     while low <= high:
+        center = (high + low) // 2
+
         if array[center] == target:
             return array[center - 1]
         if array[center] < target:
             low = center + 1
-            center = (high + low) // 2
             continue
         if array[center] > target:
             high = center - 1
-            center = (high + low) // 2
             continue
-        else:
-            return False
         
-print(binary_search(array))
+print(binary_search(array, target))
